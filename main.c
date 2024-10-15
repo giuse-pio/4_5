7e2596db68080267656d2fd067120e2364a3a3a4
giuse-pio <giupeppepio7@gmail.com>
2024-10-15 17:56:54 +0200

    fatto con il ciclo for

--- main.c
+++ main.c
@@ -2,14 +2,18 @@
 
 int main(void) {
     int nprod, prod;
-    float costotot, costoprod;
+    float costotot=0, costoprod;
     printf("inserisci il numero di prodotti che hai acquistato: ");
     scanf("%d", &nprod);
-    prod=1;
-    while (nprod >= prod ) {
+    /*while (nprod >= prod ) {
         printf(" inserisci il prezzo del prodotto: ");
         scanf("%f", &costoprod);
         prod++;
+        costotot += costoprod;
+    }*/
+    for (prod = 1; prod <= nprod; prod++) {
+        printf(" inserisci il prezzo del prodotto: ");
+        scanf("%f", &costoprod);
         costotot += costoprod;
     }
     printf("il prezzo totale e': %.2f", costotot);
